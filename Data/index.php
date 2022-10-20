573a2d7c89d3a7931fa9b404af822b2bc8fef30d
Naki00 <86929907+Naki00@users.noreply.github.com>
2022-10-20 19:54:58 +0700

    up data

---
+++ Data/index.php
@@ -0,0 +1,29 @@
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+    $userpass = "";
+    $dbname = "dataproject";
+    
+    try {
+        $dbcon = new PDO("mysql:host=$servername; dbname=$dbname", $username, $userpass);
+        $dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ATTR_ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+        echo "Faild to connect to database ". $e->getMessage();
+
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data_project</title>
+</head>
+<body>
+    
+</body>
+</html>
