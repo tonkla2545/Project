8557d1f51c398ee22bb0bc8863ea19307d6a62ae
Naki00 <86929907+Naki00@users.noreply.github.com>
2022-10-20 20:31:05 +0700

    up

--- Data/index.php
+++ Data/index.php
@@ -7,7 +7,7 @@
     
     try {
         $dbcon = new PDO("mysql:host=$servername; dbname=$dbname", $username, $userpass);
-        $dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ATTR_ERRMODE_EXCEPTION);
+        $dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e) {
         echo "Faild to connect to database ". $e->getMessage();
 
