f076caac2416f3aaf7e0bcd740329259f3e22a51
tonkla2545 <suriyakamonkaro@gmail.com>
2022-10-23 21:47:17 +0700

    Project

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ include('server.php');
             <div class="collapse navbar-collapse" id="navbarScroll">
               <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                 <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">หน้าแรก</a>
+                  <a class="nav-link active" aria-current="page" href="#">หน้าแรกก</a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="#">เกี่ยวกับ</a>
